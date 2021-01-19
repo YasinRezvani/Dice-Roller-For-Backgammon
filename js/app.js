a2bca4f38c4c837d3367d1ba01c71dfa6fcf1409
YasinRezvani <yasinrezvani2025@gmail.com>
2021-01-19 15:08:11 +0330

    fix

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+let btn = document.querySelector('.btn');
+let diceres = document.querySelector('.dice-res') ;
+let dice = document.querySelector('.dice');
+
+let dicearray1 = ['one','two','three','four','five','six'];
+let dicearray2 = ['one','two','three','four','five','six'];
+
+
 var res;function randomjs(max , min){
 
     document.getElementById('aa').innerHTML = Math.floor(Math.random() * (max-min+1) + min);
