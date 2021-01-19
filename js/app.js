027d562647c626f188f89fe6cce208e9cfab4f8a
YasinRezvani <yasinrezvani2025@gmail.com>
2021-01-20 00:46:18 +0330

    fix

--- js/app.js
+++ js/app.js
@@ -36,17 +36,5 @@ function diceresult(res1,res2){
         `
     }
 }
-/*var res;
-function randomjs(max , min){
 
-    document.getElementById('aa').innerHTML = Math.floor(Math.random() * (max-min+1) + min);
-    document.getElementById('bb').innerHTML = Math.floor(Math.random() * (max-min+1) + min);
-    
-     res = randomjs(6,1);
-}
-randomjs();
- console.log(res)
-
-
-*/
 
